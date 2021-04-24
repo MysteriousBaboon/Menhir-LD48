f09c22d5e9e96980615dd42d1b473e55ceda5225
MysteriousBaboon <mysteriousbaboon@hotmail.com>
2021-04-24 07:55:49 +0200

    Generate a map that is not visible by default but that you can activate with space

--- Assets/Scripts/Script_Generation.cs
+++ Assets/Scripts/Script_Generation.cs
@@ -4,16 +4,13 @@ using UnityEngine;
 
 public class Script_Generation : MonoBehaviour
 {                                 
-    [SerializeField]
-    private static int rows = 21; // Number of row for Tiles Generation
-    [SerializeField]
-    private static int cols = 21; // Number of Columns
+    public static int rows = 21; // Number of row for Cell Generation
+    public static int cols = 21; // Number of Columns
     public Object[] cells_Type; // Array of all type of cells
+    // Store all object in an 2D array
     public  GameObject[,] cells = new GameObject[rows, cols];
-    // Start is called before the first frame update
 
-
-    void Start()
+    void GenerateMap()
     {
         for (int row = 0; row < rows; row++)
         {
@@ -22,11 +19,25 @@ public class Script_Generation : MonoBehaviour
                 cells[row, col] = (GameObject)Instantiate(cells_Type[0]); // Randomizing the type of tile that will be generetad next
             }
         }
+
     }
 
-    // Update is called once per frame
+
+    void Start()
+    {
+        GenerateMap();
+    }
+
     void Update()
     {
-        
+         if (Input.GetKeyDown("space"))
+        {
+            
+        Color color = cells[0,0].GetComponent<SpriteRenderer>().color;
+        color.a = 1f;
+        cells[0,0].GetComponent<SpriteRenderer>().color = color;
+
+        }
+
     }
 }
